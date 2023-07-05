fc5540131b767f2a79d11bf50c988efe8cd48a13
Emory Dunn <emory@emorydunn.com>
2023-07-05 07:38:33 -0700

    Initial Commit

---
+++ SplitNavigationSample/SplitNavigationSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitNavigationSampleApp.swift
+//  SplitNavigationSample
+//
+//  Created by Emory Dunn on 7/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SplitNavigationSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
